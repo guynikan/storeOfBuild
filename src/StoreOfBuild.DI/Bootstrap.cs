ec6f6b46bd32092e8bd8ac01e9ca9eddf427e240
Ed <jr-lol@live.com>
2019-09-28 23:03:09 -0300

    adicionado DbContext

--- src/StoreOfBuild.DI/Bootstrap.cs
+++ src/StoreOfBuild.DI/Bootstrap.cs
@@ -1,13 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StoreOfBuild.Data;
 
 namespace StoreOfBuild.DI
 {
     public class Bootstrap
     {
-        public static void Configure(IServiceCollection services){
+        public static void Configure(IServiceCollection services, string connection){
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(connection));
                 
         }
     }
